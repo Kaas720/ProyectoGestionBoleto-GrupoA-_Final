fc7715ea0ae0ecc61208e744910a955ba6afbe6e
David Zorrilla Baquero <DavidZorillaBaquero@gmail.com>
2022-01-05 00:00:34 -0500

    Se creo la conexion a la base de datos remota

---
+++ Datos/Conexion.cs
@@ -0,0 +1,47 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Datos
+{
+    public class Conexion
+    {
+
+        private static string conexion_DB_externa = "Server=MYSQL5045.site4now.net;Database=db_a7eb45_sga;Uid=a7eb45_sga;Pwd=1234emelec";
+
+        private MySqlConnection sqlconnection = null;
+        public MySqlConnection Connection { get => sqlconnection; set => sqlconnection = value; }
+        public void conectar()
+        {
+            try
+            {
+                Connection = new MySqlConnection();
+                Connection.ConnectionString = conexion_DB_externa;
+                Connection.Open();
+                MessageBox.Show("Se conecto");
+            }
+            catch (SqlException x)
+            {
+                MessageBox.Show(x.ToString());
+            }
+        }
+
+        public void cerrar()
+        {
+            try
+            {
+                Connection.Close();
+                MessageBox.Show("Se Desconecto");
+            }
+            catch (Exception x)
+            {
+
+                MessageBox.Show(x.ToString());
+            }
+        }
+    }
+}
---
+++ Presentacion/InicioSeccion.cs
@@ -0,0 +1,28 @@
+﻿using Datos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion
+{
+    
+    public partial class InicioSeccion : Form
+    {
+        Conexion nuevodatos = new Conexion();
+        public InicioSeccion()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            nuevodatos.conectar();
+        }
+    }
+}

b5c935339e7b729b6ddf313f9aebbeaa7a9a0082
Ronny <Ronny@DESKTOP-SOJDK9T>
2022-01-10 22:55:28 -0500

    se agrego el modulo de cooperativa con su respectiva clase en logica de negocio.

---
+++ LogicaDeNegocios/Modulo de cooperativa/cooperativa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaDeNegocios.Modulo_de_cooperativa
+{
+    internal class cooperativa
+    {
+    }
+}

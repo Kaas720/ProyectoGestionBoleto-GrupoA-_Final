3ef9a4f385d60ea5f68f9c74e44a5c75feecaa75
Ronny <Ronny@DESKTOP-SOJDK9T>
2022-01-10 23:46:51 -0500

    clase conexion a la base de datos

---
+++ Datos/Conexion.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace Datos
+{
+    public class Conexion
+    {
+
+        private static string conexion_DB_externa = "Server=MYSQL5045.site4now.net;Database=db_a7eb45_sga;Uid=a7eb45_sga;Pwd=1234emelec";
+
+        private MySqlConnection sqlconnection = null;
+        public MySqlConnection Connection { get => sqlconnection; set => sqlconnection = value; }
+        public MySqlConnection conectar()
+        {
+            try
+            {
+                Connection = new MySqlConnection();
+                Connection.ConnectionString = conexion_DB_externa;
+                Connection.Open();
+            }
+            catch (SqlException x)
+            {
+                MessageBox.Show(x.ToString());
+
+            }
+            return Connection;
+        }
+        public void cerrar()
+        {
+            try
+            {
+                Connection.Close();
+            }
+            catch (Exception x)
+            {
+
+                MessageBox.Show(x.ToString());
+            }
+        }
+
+
+    }
+}

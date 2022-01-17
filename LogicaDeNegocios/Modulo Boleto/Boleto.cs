b7ae22b9ebe24d248ffdf48c4292bd38bf4c9dc6
SERGIO VICENTE CAMPOVERDE LEIVA <sergio.compoverdel@ug.edu.ec>
2022-01-16 21:54:14 -0500

    cambio de label

---
+++ LogicaDeNegocios/Modulo Boleto/Boleto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaDeNegocios.Modulo_Boleto
+{
+  public class Boleto
+    {
+        private string lugarOrigen;
+        private string lugarDestino;
+        private DateTime fechaSalida;
+        private DateTime horaSalida;
+        private DateTime fechaEmision;
+        private int cantidadBoleto;
+        private double precio;
+        private double impuesto;
+
+        public Boleto() { }
+        public Boleto(string lugarOrigen, string lugarDestino, DateTime fechaSalida, DateTime horaSalida, DateTime fechaEmision, int cantidadBoleto, double precio, double impuesto)
+        {
+            this.lugarOrigen = lugarOrigen;
+            this.lugarDestino = lugarDestino;
+            this.fechaSalida = fechaSalida;
+            this.horaSalida = horaSalida;
+            this.fechaEmision = fechaEmision;
+            this.cantidadBoleto = cantidadBoleto;
+            this.precio = precio;
+            this.impuesto = impuesto;
+        }
+
+        public string LugarOrigen { get => lugarOrigen; set => lugarOrigen = value; }
+        public string LugarDestino { get => lugarDestino; set => lugarDestino = value; }
+        public DateTime FechaSalida { get => fechaSalida; set => fechaSalida = value; }
+        public DateTime HoraSalida { get => horaSalida; set => horaSalida = value; }
+        public DateTime HoraSalida1 { get => horaSalida; set => horaSalida = value; }
+        public DateTime FechaEmision { get => fechaEmision; set => fechaEmision = value; }
+        public int CantidadBoleto { get => cantidadBoleto; set => cantidadBoleto = value; }
+        public double Precio { get => precio; set => precio = value; }
+        public double Impuesto { get => impuesto; set => impuesto = value; }
+
+        public override string ToString()
+        {
+            return "lugarSalida: " + lugarOrigen + "lugarDestino: " + lugarDestino + "fechaSalida: " + fechaSalida + "horaSalida" + horaSalida + "fechaEmision: " + fechaEmision +
+            "cantidadBoleto: " + cantidadBoleto + "precio: " + precio + "impuesto: " + impuesto;
+        }
+
+        //public string Guardar()
+        //{
+
+        //}
+    }
+}
--- Presentacion/UsuarioCliente/Pagar.Designer.cs
+++ Presentacion/UsuarioCliente/Pagar.Designer.cs
@@ -37,8 +37,8 @@ namespace Presentacion.UsuarioCliente
             this.btnPagar = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.BotonRetroceder = new FontAwesome.Sharp.IconButton();
+            this.guna2HtmlLabel9 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2GroupBox1.SuspendLayout();
             this.guna2Panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
@@ -60,17 +60,18 @@ namespace Presentacion.UsuarioCliente
             this.guna2GroupBox1.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(78)))), ((int)(((byte)(95)))));
             this.guna2GroupBox1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2GroupBox1.ForeColor = System.Drawing.Color.Ivory;
-            this.guna2GroupBox1.Location = new System.Drawing.Point(41, 64);
+            this.guna2GroupBox1.Location = new System.Drawing.Point(62, 98);
+            this.guna2GroupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.guna2GroupBox1.Name = "guna2GroupBox1";
             this.guna2GroupBox1.ShadowDecoration.Parent = this.guna2GroupBox1;
-            this.guna2GroupBox1.Size = new System.Drawing.Size(605, 374);
+            this.guna2GroupBox1.Size = new System.Drawing.Size(908, 575);
             this.guna2GroupBox1.TabIndex = 37;
             this.guna2GroupBox1.Text = "Información boleto";
             // 
             // guna2Button2
             // 
             this.guna2Button2.AutoRoundedCorners = true;
-            this.guna2Button2.BorderRadius = 20;
+            this.guna2Button2.BorderRadius = 31;
             this.guna2Button2.CheckedState.Parent = this.guna2Button2;
             this.guna2Button2.CustomImages.Parent = this.guna2Button2;
             this.guna2Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
@@ -81,10 +82,11 @@ namespace Presentacion.UsuarioCliente
             this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 12F);
             this.guna2Button2.ForeColor = System.Drawing.Color.White;
             this.guna2Button2.HoverState.Parent = this.guna2Button2;
-            this.guna2Button2.Location = new System.Drawing.Point(446, 227);
+            this.guna2Button2.Location = new System.Drawing.Point(669, 349);
+            this.guna2Button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.guna2Button2.Name = "guna2Button2";
             this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
-            this.guna2Button2.Size = new System.Drawing.Size(138, 42);
+            this.guna2Button2.Size = new System.Drawing.Size(207, 65);
             this.guna2Button2.TabIndex = 14;
             this.guna2Button2.Text = "CANCELAR";
             // 
@@ -92,7 +94,7 @@ namespace Presentacion.UsuarioCliente
             // 
             this.InfBoleto.Animated = true;
             this.InfBoleto.AutoRoundedCorners = true;
-            this.InfBoleto.BorderRadius = 148;
+            this.InfBoleto.BorderRadius = 229;
             this.InfBoleto.BorderStyle = System.Drawing.Drawing2D.DashStyle.Custom;
             this.InfBoleto.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.InfBoleto.CustomizableEdges.BottomLeft = false;
@@ -110,7 +112,8 @@ namespace Presentacion.UsuarioCliente
             this.InfBoleto.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.InfBoleto.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.InfBoleto.HoverState.Parent = this.InfBoleto;
-            this.InfBoleto.Location = new System.Drawing.Point(22, 55);
+            this.InfBoleto.Location = new System.Drawing.Point(33, 85);
+            this.InfBoleto.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.InfBoleto.Multiline = true;
             this.InfBoleto.Name = "InfBoleto";
             this.InfBoleto.PasswordChar = '\0';
@@ -118,14 +121,14 @@ namespace Presentacion.UsuarioCliente
             this.InfBoleto.ReadOnly = true;
             this.InfBoleto.SelectedText = "";
             this.InfBoleto.ShadowDecoration.Parent = this.InfBoleto;
-            this.InfBoleto.Size = new System.Drawing.Size(402, 299);
+            this.InfBoleto.Size = new System.Drawing.Size(603, 460);
             this.InfBoleto.TabIndex = 13;
             this.InfBoleto.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // btnPagar
             // 
             this.btnPagar.AutoRoundedCorners = true;
-            this.btnPagar.BorderRadius = 20;
+            this.btnPagar.BorderRadius = 31;
             this.btnPagar.CheckedState.Parent = this.btnPagar;
             this.btnPagar.CustomImages.Parent = this.btnPagar;
             this.btnPagar.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
@@ -136,10 +139,11 @@ namespace Presentacion.UsuarioCliente
             this.btnPagar.Font = new System.Drawing.Font("Segoe UI", 12F);
             this.btnPagar.ForeColor = System.Drawing.Color.White;
             this.btnPagar.HoverState.Parent = this.btnPagar;
-            this.btnPagar.Location = new System.Drawing.Point(446, 141);
+            this.btnPagar.Location = new System.Drawing.Point(669, 217);
+            this.btnPagar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnPagar.Name = "btnPagar";
             this.btnPagar.ShadowDecoration.Parent = this.btnPagar;
-            this.btnPagar.Size = new System.Drawing.Size(138, 42);
+            this.btnPagar.Size = new System.Drawing.Size(207, 65);
             this.btnPagar.TabIndex = 12;
             this.btnPagar.Text = "PAGAR";
             this.btnPagar.Click += new System.EventHandler(this.btnPagar_Click);
@@ -147,15 +151,16 @@ namespace Presentacion.UsuarioCliente
             // guna2Panel1
             // 
             this.guna2Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(27)))), ((int)(((byte)(32)))));
+            this.guna2Panel1.Controls.Add(this.guna2HtmlLabel9);
             this.guna2Panel1.Controls.Add(this.guna2PictureBox1);
-            this.guna2Panel1.Controls.Add(this.label4);
             this.guna2Panel1.Controls.Add(this.BotonRetroceder);
             this.guna2Panel1.Cursor = System.Windows.Forms.Cursors.PanNW;
             this.guna2Panel1.ForeColor = System.Drawing.Color.CornflowerBlue;
             this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.guna2Panel1.Name = "guna2Panel1";
             this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
-            this.guna2Panel1.Size = new System.Drawing.Size(688, 44);
+            this.guna2Panel1.Size = new System.Drawing.Size(1032, 68);
             this.guna2Panel1.TabIndex = 38;
             this.guna2Panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.guna2Panel1_MouseMove);
             // 
@@ -163,24 +168,14 @@ namespace Presentacion.UsuarioCliente
             // 
             this.guna2PictureBox1.Image = global::Presentacion.Properties.Resources.pngtree_blue_big_car_illustration_cartoon_shuttle_blue_bus_gray_glass_png_image_4543101;
             this.guna2PictureBox1.ImageRotate = 0F;
-            this.guna2PictureBox1.Location = new System.Drawing.Point(12, 4);
+            this.guna2PictureBox1.Location = new System.Drawing.Point(18, 6);
+            this.guna2PictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.guna2PictureBox1.Name = "guna2PictureBox1";
             this.guna2PictureBox1.ShadowDecoration.Parent = this.guna2PictureBox1;
-            this.guna2PictureBox1.Size = new System.Drawing.Size(66, 37);
+            this.guna2PictureBox1.Size = new System.Drawing.Size(99, 57);
             this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.guna2PictureBox1.TabIndex = 41;
             this.guna2PictureBox1.TabStop = false;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label4.Location = new System.Drawing.Point(84, 16);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(214, 18);
-            this.label4.TabIndex = 39;
-            this.label4.Text = "BIENVENIDO A VIAJEROS S.A";
             // 
             // BotonRetroceder
             // 
@@ -195,23 +190,37 @@ namespace Presentacion.UsuarioCliente
             this.BotonRetroceder.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.BotonRetroceder.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.BotonRetroceder.IconSize = 37;
-            this.BotonRetroceder.Location = new System.Drawing.Point(629, 0);
+            this.BotonRetroceder.Location = new System.Drawing.Point(944, 0);
+            this.BotonRetroceder.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.BotonRetroceder.Name = "BotonRetroceder";
-            this.BotonRetroceder.Size = new System.Drawing.Size(59, 44);
+            this.BotonRetroceder.Size = new System.Drawing.Size(88, 68);
             this.BotonRetroceder.TabIndex = 40;
             this.BotonRetroceder.UseVisualStyleBackColor = false;
             this.BotonRetroceder.Click += new System.EventHandler(this.BotonRetroceder_Click);
             // 
+            // guna2HtmlLabel9
+            // 
+            this.guna2HtmlLabel9.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.guna2HtmlLabel9.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.guna2HtmlLabel9.Location = new System.Drawing.Point(108, 24);
+            this.guna2HtmlLabel9.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.guna2HtmlLabel9.Name = "guna2HtmlLabel9";
+            this.guna2HtmlLabel9.Size = new System.Drawing.Size(445, 28);
+            this.guna2HtmlLabel9.TabIndex = 42;
+            this.guna2HtmlLabel9.Text = "Sistema de Gestion de Boletos de AutoBuses";
+            // 
             // Pagar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Presentacion.Properties.Resources.salvapantallas_textura_elegante_blanco_23_2148421855;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(687, 450);
+            this.ClientSize = new System.Drawing.Size(1030, 692);
             this.Controls.Add(this.guna2Panel1);
             this.Controls.Add(this.guna2GroupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Pagar";
             this.Text = "Pagar";
             this.guna2GroupBox1.ResumeLayout(false);
@@ -230,8 +239,8 @@ namespace Presentacion.UsuarioCliente
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
         private FontAwesome.Sharp.IconButton BotonRetroceder;
         private Guna.UI2.WinForms.Guna2TextBox InfBoleto;
-        private System.Windows.Forms.Label label4;
         private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
         private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel9;
     }
 }

98558b030a5bdae86285372a02cd43d01b581d5f
Kevin Alexander Arevalo <Kaas7520@gmail.com>
2022-01-19 22:01:26 -0500

    s

--- Presentacion/Program.cs
+++ Presentacion/Program.cs
@@ -19,8 +19,8 @@ namespace Presentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(principal = new Principal_Usuario());
-            Application.Run(new VentanaAdministrador());
+            Application.Run(principal = new Principal_Usuario());
+            //Application.Run(new VentanaAdministrador());
         }
     }
 }

48b52c8c0c9e485d8095af0b118197ca9ea17d05
Kevin Alexander Arevalo <Kaas7520@gmail.com>
2022-01-19 23:44:09 -0500

    Se crearon prubas unitaris

---
+++ Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("93bb655c-426f-4c64-ad1f-4fed49e49fce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using LogicaDeNegocios;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PruebaInicioSeccion()
+        {
+            ConsultasProcedimientos consultasProcedimientos = new ConsultasProcedimientos();
+            bool result = consultasProcedimientos.Login("Kaas7520@hotmail.com", "1234", 1);
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void PruebaValidacionCedula()
+        {
+            Validacion validacion = new Validacion();
+            bool result = validacion.ValidarCedula("0302527742");
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void PruebaValidacionTelefono()
+        {
+            Validacion validacion = new Validacion();
+            bool result = validacion.ValidarTelefono("0991887965");
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void PruebaValidacioCorreo()
+        {
+            Validacion validacion = new Validacion();
+            bool result = validacion.validarEmail("Kevin7520@hotmail.com");
+            Assert.AreEqual(true, result);
+        }
+    }
+}

acfb2f97e61b9979f76d0c555cb23896ff4d1c65
SERGIO VICENTE CAMPOVERDE LEIVA <sergio.compoverdel@ug.edu.ec>
2022-02-05 07:15:40 -0500

    pruebas

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -7,16 +7,64 @@ namespace Test
     [TestClass]
     public class UnitTest1 {
 
-        AdmClienteProcedimiento cliente = new AdmClienteProcedimiento();
+        /// <summary>
+        /// CP-001
+        /// Registro de administrador
+        /// Prueba unitaria que registra al administrador en la cual se envia los datos a ser registrados
+        /// </summary>
+        AdmAdministrador adm = new AdmAdministrador();
+        [TestMethod]
+        public void RegistroAdministrador()
+        {
+            CredencialUsuario credencial = new CredencialUsuario("deiber@gmail.com", "Deiber1234", 1);
+            Administrador admin = new Administrador("0968387363", "Willian Encalada", "Masculino", "0987466344", "12345@88. @", 920.45, credencial);
+            var resultadoEsperado = adm.RegistrarAdministrador(admin);
+            var resultadoObtenido = true;
+            Assert.AreEqual(resultadoEsperado, resultadoObtenido);
+        }
+
+        /// <summary>
+        /// CP-002
+        /// Registro de chofer
+        /// Prueba unitaria que registra el chofer en la cual se envia los datos a ser registrados
+        /// </summary>
+        AdmChofer adf = new AdmChofer();
+        [TestMethod]
+        public void RegistroChofer()
+        {
+            CredencialUsuario credencial = new CredencialUsuario("manuel@gmail.com", "manuel123", 2);
+            Chofer chofer = new Chofer("1292938774", "Manuel Mendieta", "Masculino", "0976453763", "1274738840", 845.50, credencial);
+            var resultadoEsperado = adf.RegistrarChofer(chofer);
+            var resultadoObtenido = true;
+            Assert.AreEqual(resultadoEsperado, resultadoObtenido);
+        }
+
+        /// <summary>
+        /// CP-003
+        /// Registro de vendedor
+        /// Prueba unitaria que registra el vendedor en la cual se envia los datos a ser registrados
+        /// </summary>
+        AdmVendedor adv = new AdmVendedor();
+        [TestMethod]
+        public void RegistroVendedor()
+        {
+            CredencialUsuario credencial = new CredencialUsuario("maria@hotmail.com", "marial12345", 3);
+            Vendedor vendedor = new Vendedor("1938373374", "Maria Hurtado", "Femenino", "0984473847", 586.98, credencial);
+            var resultadoEsperado = adv.RegistrarVendedor(vendedor);
+            var resultadoObtenido = true;
+            Assert.AreEqual(resultadoEsperado, resultadoObtenido);
+        }
+
         /// <summary>
         /// CP-004
+        /// Prueba unitaria de registro de cliente en la cual se envia los datos a ser registrados
         /// </summary>
-        /// 
-
+        AdmClienteProcedimiento cliente = new AdmClienteProcedimiento();
         [TestMethod]
         public void RegistroCliente()
         {
-            Cliente client = new Cliente("1234567890", "Jorge Velez", "Masculino", "0987765432", "jorge@gmail.com", "velez123",4);
+            CredencialUsuario credencial = new CredencialUsuario("jose@gmail.com", "jose123456", 4);
+            Cliente client = new Cliente("1263722998", "Jose Mendez", "Masculino", "0983646533", credencial);
             var resultadoEsperado = cliente.RegistrarCliente(client);
             var resultadoObtenido = true;
             Assert.AreEqual(resultadoEsperado, resultadoObtenido);

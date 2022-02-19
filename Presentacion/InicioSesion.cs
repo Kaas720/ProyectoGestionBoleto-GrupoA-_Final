66fc6cb306762b7ccccbac90dcf9a9696f462b98
SERGIO VICENTE CAMPOVERDE LEIVA <sergio.compoverdel@ug.edu.ec>
2022-02-19 15:30:46 -0500

    Se cambio el nombre de la lista

--- Presentacion/InicioSesion.cs
+++ Presentacion/InicioSesion.cs
@@ -41,11 +41,11 @@ namespace Presentacion
         }
         private void ConsultarLoginBD(string correo, string password)
         {
-            List<int> RolUsuario2 = consulta.Login(correo, password);
-            if (RolUsuario2[0] > 0)
+            List<int> Rol = consulta.Login(correo, password);
+            if (Rol[0] > 0)
             {
                 this.Close();
-                switch (RolUsuario2[0])
+                switch (Rol[0])
                 {
                     case 1: 
                         VentanaAdministrador ventanaAdministrador = new VentanaAdministrador();
